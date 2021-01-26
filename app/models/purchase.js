d568fb0af33b449757ec92f5900c09ddfef5323a
zanhorst <zanhorst@gmail.com>
2021-01-26 13:12:25 -0500

    First commit
    Co-contributor: Shane shaneajames123@gmail.com
    Co-contributor: Dylan ddogstricker@gmail.com
    Co-contributor: Alex alexjm825@gmail.com
    added purchase schema and modified user schema. also added purchase
    routes.

---
+++ app/models/purchase.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const purchaseSchema = new mongoose.Schema({
+  location: {
+    name: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: false
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true }
+})
+
+module.exports = mongoose.model('Purchase', purchaseSchema)
